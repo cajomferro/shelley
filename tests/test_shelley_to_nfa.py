a593adc0fede32d7d1c6a04b56574e633e3b5821
cajomferro <cajomferro@gmail.com>
2020-03-27 14:12:12 +0000

    improved tests

--- tests/test_shelley_to_nfa.py
+++ tests/test_shelley_to_nfa.py
@@ -5,8 +5,7 @@ from .context import shelley
 from typing import Dict
 
 import yaml
-
-from karakuri.regular import Regex, nfa_to_regex, dfa_to_nfa
+from karakuri.regular import Regex, nfa_to_regex, dfa_to_nfa, Char, Concat, Union, Nil
 
 from shelley.automata import Device as AutomataDevice, check_valid_device, CheckedDevice, InvalidBehavior
 from shelley.ast.devices import Device as ShelleyDevice
@@ -29,94 +28,100 @@ def get_regex_dict(triggers: Triggers) -> Dict[str, Regex]:
 
 def get_automata_device(name: str) -> AutomataDevice:
     shelley_device = get_shelley_device(name)
-    return AutomataDevice(events=shelley_device.get_all_events().list_str(),
+    return AutomataDevice(start_event='begin',
+                          events=shelley_device.get_all_events().list_str(),
                           behavior=shelley_device.behaviors.as_list_tuples(),
                           components=shelley_device.components.components_to_devices,
                           triggers=get_regex_dict(shelley_device.triggers))
 
 
-def test_button_automata():
+def test_button():
     expected = AutomataDevice(
-        start_event = 'begin',
-        events = ['begin', 'pressed', 'released'],
-        behavior = [
+        start_event='begin',
+        events=['begin', 'pressed', 'released'],
+        behavior=[
             ('begin', 'pressed'),
             ('pressed', 'released'),
             ('released', 'pressed'),
         ],
         components={},
-        triggers = {},
+        triggers={
+            'pressed': Nil,
+            'released': Nil,
+        },
     )
     assert expected == get_automata_device('button')
 
 
-def test_led_automata():
-    automata = get_automata_device('led')
+def test_led():
+    expected = AutomataDevice(
+        start_event='begin',
+        events=['begin', 'on', 'off'],
+        behavior=[
+            ('begin', 'on'),
+            ('on', 'off'),
+            ('off', 'on'),
+        ],
+        components={},
+        triggers={
+            'on': Nil,
+            'off': Nil,
+        },
+    )
+    assert expected == get_automata_device('led')
 
-    assert automata.events == ['begin', 'on', 'off']
-    assert automata.behavior == [('begin', 'on'), ('on', 'off'),
-                                 ('off', 'on')]
-    assert automata.components == {}
-
-    result_str = ""
-    for key in automata.triggers:
-        regex = automata.triggers[key]
-        result_str += ("{0}: {1}\n".format(key, regex.to_string()))
-
-    assert result_str.strip() == 'on: []\noff: []'
-
-    checked_led = check_valid_device(automata, {})
-    assert isinstance(checked_led, CheckedDevice)
-
-
-def test_timer_automata():
-    automata = get_automata_device('timer')
-
-    assert automata.events == ['begin', 'started', 'canceled', 'timeout']
-    assert automata.behavior == [('begin', 'started'), ('started', 'canceled'), ('started', 'timeout'),
-                                 ('canceled', 'started'), ('timeout', 'started')]
-    assert automata.components == {}
-
-    result_str = ""
-    for key in automata.triggers:
-        regex = automata.triggers[key]
-        result_str += ("{0}: {1}\n".format(key, regex.to_string()))
-
-    assert result_str.strip() == 'started: []\ncanceled: []\ntimeout: []'
-
-    checked_timer = check_valid_device(automata, {})
-    assert isinstance(checked_timer, CheckedDevice)
+    #    checked_led = check_valid_device(automata, {})
+    #    assert isinstance(checked_led, CheckedDevice)
 
 
-def test_buttonon_automata():
-    automata = get_automata_device('buttonon')
-
-    assert automata.events == ['begin', 'on']
-    assert automata.behavior == [('begin', 'on'), ('on', 'on')]
-    assert automata.components == {'b': 'Button'}
-
-    result_str = ""
-    for key in automata.triggers:
-        regex = automata.triggers[key]
-        result_str += ("{0}: {1}\n".format(key, regex.to_string(app_str=lambda x, y: x + " ; " + y)))
-
-    # begin: b.begin ; ledA.begin ; ledB.begin ; t.begin
-    expected_str = 'on: b.pressed ; b.released'
-
-    assert result_str.strip() == expected_str
-
-    known_devices = {'Button': check_valid_device(get_automata_device('button'), {})}
-
-    checked_automata = check_valid_device(automata, known_devices)
-    print()
-    print(checked_automata.dfa)
-    #print(dfa_to_nfa(checked_automata.dfa))
-    print(nfa_to_regex(dfa_to_nfa(checked_automata.dfa)).to_string(app_str=lambda x, y: x + " ; " + y))
-
-    #assert isinstance(checked_automata, CheckedDevice)
+def test_timer():
+    expected = AutomataDevice(
+        start_event='begin',
+        events=['begin', 'started', 'canceled', 'timeout'],
+        behavior=[
+            ('begin', 'started'),
+            ('started', 'canceled'),
+            ('started', 'timeout'),
+            ('canceled', 'started'),
+            ('timeout', 'started')
+        ],
+        components={},
+        triggers={
+            'started': Nil,
+            'canceled': Nil,
+            'timeout': Nil
+        },
+    )
+    assert expected == get_automata_device('timer')
 
 
-def test_desklamp_automata():
+def test_smartbutton1():
+    expected = AutomataDevice(
+        start_event='begin',
+        events=['begin', 'on'],
+        behavior=[
+            ('begin', 'on'),
+            ('on', 'on')
+        ],
+        components={'b': 'Button'},
+        triggers={
+            'on': Concat(Char('b.pressed'), Char('b.released'))
+        },
+    )
+    assert expected == get_automata_device('smartbutton1')
+
+    # known_devices = {'Button': check_valid_device(get_automata_device('button'), {})}
+    #
+    # checked_automata = check_valid_device(automata, known_devices)
+    # print()
+    # print(checked_automata.dfa)
+    # # print(dfa_to_nfa(checked_automata.dfa))
+    # print(nfa_to_regex(dfa_to_nfa(checked_automata.dfa)).to_string(app_str=lambda x, y: x + " ; " + y))
+
+    # assert isinstance(checked_automata, CheckedDevice)
+
+
+def test_desklamp():
     automata = get_automata_device('desklamp')
 
     assert automata.events == ['begin', 'level1', 'standby1', 'level2', 'standby2']

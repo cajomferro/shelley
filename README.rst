c5aef07d834b91b380a986ba6faac9fd6a9d5d43
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2020-04-03 09:22:02 -0400

    Added visualization.

--- README.rst
+++ README.rst
@@ -8,19 +8,22 @@ Run compiler
 .. code-block:: shell
 
    # show all otions
-   python -m appcompiler -h
+   python3 -m appcompiler -h
 
    # compile a device without dependencies (uses)
-   python -m appcompiler -d examples/button.yaml
+   python3 -m appcompiler -d examples/button.yaml
 
    # compile a device with dependencies (uses)
-   python -m appcompiler -u examples/compiled/button.sc:Button examples/compiled/led.sc:Led examples/compiled/timer.sc:Timer -d examples/desklamp.yaml
+   python3 -m appcompiler -u examples/button.sc:Button examples/led.sc:Led examples/timer.sc:Timer -d examples/desklamp.yaml
 
    # user-defined output folder (-o), verbose (-v)
-   python -m appcompiler -o examples/compiled  -u examples/compiled/button.sc:Button examples/compiled/led.sc:Led examples/compiled/timer.sc:Timer -d examples/desklamp.yaml -v
+   python3 -m appcompiler -o examples/compiled  -u examples/compiled/button.sc:Button examples/compiled/led.sc:Led examples/compiled/timer.sc:Timer -d examples/desklamp.yaml -v
 
    # compile a device in binary mode
-   python -m appcompiler -b -d examples/button.yaml
+   python3 -m appcompiler -b -d examples/button.yaml
+
+   # visualize a compiled device
+   python3 viz.py < examples/desklamp.scy | xdot -
 
 .. warning:: To compile composite devices, please compile all dependency components first.
 
---
+++ viz.py
@@ -0,0 +1,44 @@
+import graphviz
+import yaml
+
+def visualize_graph(automaton):
+    dot = graphviz.Digraph()
+    nodes = set(k for edge in automaton["edges"] for k in (edge["src"], edge["dst"]))
+    nodes.add(automaton["start_state"])
+    nodes.union(automaton["accepted_states"])
+    accepted = set(automaton["accepted_states"])
+    for node in sorted(nodes):
+        kwargs = {"shape": "circle"}
+        if node == automaton["start_state"] and node in accepted:
+            kwargs["shape"] = "doubleoctagon"
+        elif node == automaton["start_state"]:
+            kwargs["shape"] = "octagon"
+        elif node in accepted:
+            kwargs["shape"] = "doublecircle"
+        dot.node(node, **kwargs)
+    # Group by edges:
+    edges = {}
+    for edge in automaton["edges"]:
+        pair = edge["src"], edge["dst"]
+        outs = edges.get(pair, None)
+        if outs is None:
+            edges[pair] = outs = []
+        outs.append(edge["char"])
+    # Create an edge per edge
+    for ((src, dst), chars) in sorted(edges.items()):
+        chars = sorted(map(str, chars))
+        dot.edge(src, dst, label=", ".join(chars))
+
+    return dot
+
+def save_graph(fp, filename):
+    d = yaml.safe_load(fp)
+    dot = visualize_graph(d)
+    #d.save(filename)
+    print(dot)
+
+def main():
+    import sys
+    save_graph(sys.stdin, "/dev/stdout")
+
+main()

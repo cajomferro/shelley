f5b8dffb1ef392046d2d5870294a8725a0e52357
cajomferro <cajomferro@gmail.com>
2020-04-06 16:28:57 +0100

    simplification to appvizviewer

--- appvizviewer/__main__.py
+++ appvizviewer/__main__.py
@@ -1,6 +1,4 @@
-import logging
 import yaml
-import sys
 import os
 import argparse
 
@@ -8,42 +6,23 @@ from .context import shelley
 
 from shelley.automata.view import automaton2dot
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Vizualize compiled files as state diagrams')
-    parser.add_argument("-o", "--outdir", help="path to dot files")
-    parser.add_argument("-f", "--outformat", help="output format (pdf, png, svg, ...)")
-    parser.add_argument("-p", "--outprint", help="print output for xdot", action='store_true')
-    parser.add_argument("-i", '--input', help="Path to the compiled yaml file (.scy or .scb)", required=True)
+    parser.add_argument("input", help="Path to the compiled yaml file (.scy or .scb)")
+    parser.add_argument("-o", "--output", help="Path to the generated dot file")
     return parser
 
 
-def save_and_view_graph(infilepath, outfilepath, outformat, outprint=False):
-    with open(infilepath, 'r') as f:
-        d = yaml.load(f, Loader=yaml.FullLoader)
-    dot = automaton2dot(d)
-    if outformat is not None:
-        dot.format = outformat
-    dot.render(outfilepath, view=False)
-    if outprint:
-        print(dot)
-
-
 def main(args):
-    filepath = os.path.realpath(args.input)  # --device
-    basedir = os.path.dirname(filepath)
-    filename = os.path.basename(filepath)
 
-    if args.outdir is None:
-        outdir = basedir
-    else:
-        outdir = args.outdir
-    outfilepath = os.path.join(outdir, '{0}.gv'.format(filename))
+    with open(args.input, 'r') as f:
+        d = yaml.load(f, Loader=yaml.FullLoader)
 
-    save_and_view_graph(filepath, outfilepath, args.outformat, args.outprint)
+    dot = automaton2dot(d)
+
+    with open(args.output, 'w') as fp:
+        print(dot, file=fp)
 
 
 if __name__ == "__main__":

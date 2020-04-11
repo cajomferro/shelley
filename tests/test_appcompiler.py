8e9592916e9b4e37ba4be52475b1695a673bc92b
cajomferro <cajomferro@gmail.com>
2020-04-11 19:40:33 +0100

    merge all tests related to appcompiler in a single file

--- tests/test_appcompiler.py
+++ tests/test_appcompiler.py
@@ -1,24 +1,75 @@
-import os, pytest
+import os
+import pytest
+import yaml
 
+from .context import shelley
 from .context import appcompiler
 
 import appcompiler
-from shelley.ast import devices
+from shelley.automata import Device as AutomataDevice, AssembledDevice, assemble_device, CheckedDevice, check_traces
+from shelley.ast.devices import Device as ShelleyDevice
+from shelley.shelley2automata import shelley2automata
+from shelley.yaml2shelley import create_device_from_yaml
 
 EXAMPLES_PATH = 'tests/input'
 COMPILED_PATH = 'tests/input/compiled'
 
 
-#
-# def test_find_yml():
-#     with pytest.raises(CompilationError) as exc_info:
-#         _find_compiled_device('examples/compiled/button.yml')
-#     assert "Invalid file: examples/compiled/button.yml. Expecting extension .scy or .scb but found yml!" == str(
-#         exc_info.value)
-#
-#     with pytest.raises(CompilationError) as exc_info:
-#         _find_compiled_device('examples/compiled/button.scb')
-#     assert "examples/compiled/button.scb not found! Please compile it first!" == str(exc_info.value)
+### TEST ASSEMBLE DEVICE ###
+
+def get_shelley_device(name: str) -> ShelleyDevice:
+    with open('tests/input/{0}.yml'.format(name), 'r') as stream:
+        yaml_code = yaml.load(stream, Loader=yaml.BaseLoader)
+    return create_device_from_yaml(yaml_code)
+
+
+def test_button():
+    shelley_device: ShelleyDevice = get_shelley_device('button')
+    automata: AutomataDevice = shelley2automata(shelley_device)
+
+    assembled_button: AssembledDevice = assemble_device(automata, {})
+    assert type(assembled_button.external) == CheckedDevice
+
+    # # test macro traces
+    # check_traces(assembled_button.external.nfa, shelley_device.test_macro['ok'],
+    #              shelley_device.test_macro['fail'])  # macro
+    #
+    # # test micro traces
+    # check_traces(assembled_button.internal, shelley_device.test_micro['ok'],
+    #              shelley_device.test_micro['fail'])  # micro
+
+
+def test_smart_button():
+    checked_button = assemble_device(shelley2automata(get_shelley_device('button')), {}).external
+    assert type(checked_button) == CheckedDevice
+
+    shelley_device = get_shelley_device('smartbutton1')
+    automata = shelley2automata(shelley_device)
+    assembled_smartbutton: AssembledDevice = assemble_device(automata, {'Button': checked_button})
+    assert type(assembled_smartbutton.external) == CheckedDevice
+
+    with pytest.raises(ValueError) as exc_info:
+        # test micro traces
+        check_traces(assembled_smartbutton.internal, {"good": ["b.released", "b.pressed"]}, {})  # micro
+
+    assert str(exc_info.value) == "Unaccepted valid trace: good : ['b.released', 'b.pressed']"
+
+    # # test macro traces
+    # check_traces(assembled_smartbutton.external.nfa, shelley_device.test_macro['ok'],
+    #              shelley_device.test_macro['fail'])  # macro
+    #
+    # # test micro traces
+    # check_traces(assembled_smartbutton.internal, shelley_device.test_micro['ok'],
+    #              shelley_device.test_micro['fail'])  # micro
+
+
+def test_desklamp():
+    dev = shelley2automata(get_shelley_device('desklamp'))
+    known_devices = {'Led': assemble_device(shelley2automata(get_shelley_device('led')), {}).external,
+                     'Button': assemble_device(shelley2automata(get_shelley_device('button')), {}).external,
+                     'Timer': assemble_device(shelley2automata(get_shelley_device('timer')), {}).external}
+    assert type(assemble_device(dev, known_devices).external) == CheckedDevice
+
 
 ### TEST ARGPARSE ###
 

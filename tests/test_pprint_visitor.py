f50cfe36323587f47dae91338f3e989b6ff7c3ee
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2020-05-29 20:11:18 -0400

    Made pprint pass.

--- tests/test_pprint_visitor.py
+++ tests/test_pprint_visitor.py
@@ -79,10 +79,10 @@ def test_pprint_desklamp() -> None:
   components:
     Led ledA, Led ledB, Button b, Timer t
   triggers:
-    level1: ( b.pressed ; ( b.released ; ( ledA.on ; t.started ) ) )
-    level2: ( b.pressed ; ( b.released ; ( ( ( t.canceled ; ledB.on ) xor ( ledB.on ; t.canceled ) ) ; t.started ) ) )
-    standby1: ( t.timeout ; ledA.off )
-    standby2: ( ( ( b.pressed ; ( b.released ; t.canceled ) ) xor t.timeout ) ; ( ( ledB.off ; ledA.off ) xor ( ledA.off ; ledB.off ) ) )
+    level1: b.pressed ; b.released ; ledA.on ; t.started
+    level2: b.pressed ; b.released ; ( t.canceled ; ledB.on xor ledB.on ; t.canceled ) ; t.started
+    standby1: t.timeout ; ledA.off
+    standby2: ( b.pressed ; b.released ; t.canceled xor t.timeout ) ; ( ledB.off ; ledA.off xor ledA.off ; ledB.off )
 
 """
 

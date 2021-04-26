d3553d97a29296aa0e2ad5cb22af051276c9bd03
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2021-04-26 12:36:25 -0400

    Work on svm generation.

--- shelley/shelleyv/main.py
+++ shelley/shelleyv/main.py
@@ -6,6 +6,68 @@ from karakuri import regular
 from shelley.automata.view import fsm2dot, fsm2tex
 from pathlib import Path
 import json
+# LTLSPEC (action=level1) -> (action=standby1 | action=level1);
+def svm_dump(state_diagram, fp):
+    to_state = lambda x: "Q_" + str(x)
+    to_act = lambda x: x if x is None else x.replace(".", "_")
+    to_edge = lambda act, dst: act + "." + dst if act is not None else dst
+    INDENT = "    "
+    def decl_var(name, values):
+        if isinstance(values, str):
+            values_str = values
+        else:
+            values_str = "{" +  ", ".join(str(x) for x in values) + "}"
+        print(f"{INDENT}{name}: {values_str};", file=fp)
+    def add_edge2(src, char, dst):
+        print(" " * 10, "state", "=", src, end='', file=fp)
+        if char is not None:
+            print(" &", "action =", to_act(edge["char"]), end="", file=fp)
+        print(" :", dst, ";", file=fp)
+    def add_edge2(src, char, dst):
+        act = f" & action={char}" if char is not None else ""
+        done = "yes" if dst in state_diagram["accepted_states"] else "no"
+        print(f"( (state={src}{act}) -> (state={dst}) & action=action & end={done})", file=fp)
+    def add_edge(src, char, dst):
+        act = f" & action={char}" if char is not None else ""
+        print(f"state={src}{act}: {dst};", file=fp)
+    def init_var(name, value):
+        print(f"{INDENT}init({name}) := {value};", file=fp)
+    acts = list(set(to_act(edge["char"]) for edge in state_diagram["edges"] if edge["char"] is not None))
+    acts.sort()
+    print("MODULE main", file=fp)
+    print("VAR", file=fp)
+    decl_var("end", "boolean")
+    decl_var("action", acts)
+
+    states = list(
+        set( x["src"] for x in state_diagram["edges"] ).union(
+        set( x["src"] for x in state_diagram["edges"] ))
+    )
+    states.sort()
+    states.append("eof")
+    decl_var("state", states)
+    print("ASSIGN", file=fp)
+    init_var("action", "{" + ", ".join(acts) + "}")
+    init_var("state", state_diagram["start_state"])
+    init_var("end", "TRUE" if state_diagram["start_state"] in state_diagram["accepted_states"] else "FALSE")
+    print(f"{INDENT}next(state) := case", file=fp)
+    for edge in state_diagram["edges"]:
+        print(f"{INDENT}{INDENT}", end="", file=fp)
+        src, char, dst = edge["src"], edge["char"], edge["dst"]
+        add_edge(src, char, dst)
+    print(f"{INDENT}{INDENT}TRUE: eof;", file=fp)
+    print(f"{INDENT}esac;", file=fp)
+    print(f"{INDENT}next(end) := case", file=fp)
+    for edge in state_diagram["edges"]:
+        src, char, dst = edge["src"], edge["char"], edge["dst"]
+        if dst in state_diagram["accepted_states"]:
+            act = f" & action={char}" if char is not None else ""
+            accepted = state_diagram["accepted_states"]
+            print(f"{INDENT}{INDENT}state={src}{act}: TRUE; -- dst={dst} in {accepted}", file=fp)
+    print(f"{INDENT}{INDENT}TRUE : FALSE;", file=fp)
+    print(f"{INDENT}esac;", file=fp)
+    print("LTLSPEC action = level1 -> X(action = standby1 -> X (end = TRUE));", file=fp)
+
 
 def mclr2_dump(state_diagram, fp):
     to_state = lambda x: "Q_" + str(x)
@@ -160,6 +222,11 @@ def main() -> None:
         return
     if args.format == "mclr2":
         mclr2_dump(n.as_dict(flatten=True), fp)
+        return
+    if args.format == "svm":
+        # if not args.no_epsilon and not args.dfa:
+        #     parser.error("Option '--output svm' requires either '--dfa' or '--no-epsilon'")
+        svm_dump(n.as_dict(flatten=True), fp)
         return
 
 

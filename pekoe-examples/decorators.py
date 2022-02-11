a848535fbf02a59757f7aa86d80a32911dbea800
Carlos Mão de Ferro <cajomferro@gmail.com>
2022-02-11 13:15:48 +0000

    removing pekoe examples

--- pekoe-examples/decorators.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import MagicMock, patch
-
-# decorators
-device = MagicMock()
-callback = MagicMock()
-action = MagicMock()
-raise_event = MagicMock()
-program = MagicMock()
-interface = MagicMock()
